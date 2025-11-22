4a47bda8cc74c94bfbfe2222c50459a1f090523c
Garrett Corbin <jgarrettcorbin@gmail.com>
2025-11-21 17:24:58 -0800

    urlencoded

--- index.ts
+++ index.ts
@@ -57,6 +57,8 @@ const PORT = 3010;
 
 app.use(express.json({ limit: "10mb" }));
 app.use(express.text({ type: "*/*", limit: "10mb" }));
+app.use(express.urlencoded({ extended: true }));
+
 
 app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname, "index.html"));
